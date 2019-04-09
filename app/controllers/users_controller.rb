696755df01fcbe2a603a390c12f844683f79514d
Grace Choi <oiograceoio@gmail.com>
2019-04-09 16:15:40 +0000

    modified controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,22 @@ class UsersController < ApplicationController
    end
  end
 
+ get '/login' do
+    if logged_in?
+      redirect to '/exercises'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/exercises'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
