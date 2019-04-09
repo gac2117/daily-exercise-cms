2e5b1b538bae6520435d0b26731ade85f0820ba3
Grace Choi <oiograceoio@gmail.com>
2019-04-09 19:38:03 +0000

    modified users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      binding.pry
-      redirect to '/exercises'
+      redirect to '/users/:username'
     else
       redirect to '/login'
     end
