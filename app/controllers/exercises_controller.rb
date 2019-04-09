c6986f5a49d88f0446c3b1ad1ded2359ea51e018
Grace Choi <oiograceoio@gmail.com>
2019-04-09 20:18:11 +0000

    modified exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -25,10 +25,10 @@ class ExercisesController < ApplicationController
           redirect to '/exercises/new'
         else
           @exercise = Exercise.create(name: params[:name], minutes: params[:minutes], date: params[:date])
-          binding.pry
           @current_user = User.find_by_id(session[:user_id])
           @exercise.user_id = @current_user.id
           @exercise.save
+          redirect to "/users/#{@current_user.username}"
         end
       end
     end
