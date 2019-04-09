521df2c5aeb303bb9a39fb988b1816d54b8ba87c
Grace Choi <oiograceoio@gmail.com>
2019-04-09 16:26:44 +0000

    modified exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,3 +1,33 @@
 class ExercisesController < ApplicationController
 
+  get '/exercises' do
+    @exercises = Exercise.all
+    if logged_in?
+      erb :'/exercises/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/exercises/new' do
+      if logged_in?
+        erb :'/exercises/new'
+      else
+        redirect to '/login'
+      end
+    end
+
+    post '/exercises/new' do
+      if logged_in?
+        if params[:name] == "" || params[:minutes] == "" || params[:date] == ""
+          redirect to '/exercises/new'
+        else
+          @exercise = Exercise.create(name: params[:name], minutes: params[:minutes], date: params[:date])
+          @current_user = User.find_by_id(session[:user_id])
+          @exercise.user_id = @current_user.id
+          @exercise.save
+        end
+      end
+    end
+
 end
